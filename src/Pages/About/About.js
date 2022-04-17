c62784838bad534de664afdfe66f52df27bf66bc
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-04-18 02:45:48 +0600

    about-me-updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,9 +5,9 @@ const About = () => {
     return (
         <div>
             <img src={myImage} alt="" width={300} />
-            <h2>Muhammad Al Amin</h2>
-            <div className=''>
-            <p>Assalamu Alaikum! My aim is to become a self-taught software Engineer. I will work on it until I get there. Currently, I'm learning web development at Programming Hero. It's a very good place to learn web development. They are very caring. So my 1st helping hand is Programming Hero to become a software Engineer. Insha Allah I will learn consistently to fulfil my dream. Though it's not an easy job, I will try my best.</p>
+            <h2 className='mt-4'>Muhammad Al Amin</h2>
+            <div>
+            <p className='text-start m-4'>Assalamu Alaikum! My aim is to become a self-taught software Engineer. I will work on it until I get there. Currently, I'm learning web development at Programming Hero. It's a very good place to learn web development. They are very caring. So my 1st helping hand is Programming Hero to become a software Engineer. Insha Allah I will learn consistently to fulfil my dream. Though it's not an easy job, I will try my best. I got a learning habit from Programming Hero and I will keep it until I fulfil my dream. I will Code, Practice, Practice and Practice. Maybe It will make me perfect and give me energy to learn something new. That's all. Please keep me in your prayer.</p>
             </div>
         </div>
     );
