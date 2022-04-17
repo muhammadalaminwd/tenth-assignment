b6fc2844f11f8c77cbf99c316606d51c54bc1b7d
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-04-17 23:22:54 +0600

    footer-added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,7 +5,10 @@ const About = () => {
     return (
         <div>
             <img src={myImage} alt="" width={300} />
-            <h2>Assalamu Alaikum! My name is Muhammad Al Amin. My aim is to become a self taught software Engineer.</h2>
+            <h2>Muhammad Al Amin</h2>
+            <div className=''>
+            <p>Assalamu Alaikum! My aim is to become a self-taught software Engineer. I will work on it until I get there. Currently, I'm learning web development at Programming Hero. It's a very good place to learn web development. They are very caring. So my 1st helping hand is Programming Hero to become a software Engineer. Insha Allah I will learn consistently to fulfil my dream. Though it's not an easy job, I will try my best.</p>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    return (
-        <footer className='text-center m-5 bg-black d-flex align-items-center justify-content-center'>
-        <div className='conatiner d-flex justify-content-between align-items-center'>
-        <div className='copyright'>
-        <p><small className='text-white'>copyright @{year}</small></p>
+  const today = new Date();
+  const year = today.getFullYear();
+  return (
+    <footer className="bg-dark">
+      <div className="mt-5 pt-5 pb-5 footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-5 col-xs-12 about-company">
+              <p className="pr-5 text-white-50">
+                Let's make your study fun with your virtual teachers. We can provide you the best teaching service. Our teachers will care you 24/7. So enroll now!
+              </p>
+            </div>
+            <div className="col-lg-3 col-xs-12 links">
+              <ul className="m-0 p-0 text">
+                <li>
+                  - <a class="text-decoration-none" href="/services">Services</a>
+                </li>
+                <li>
+                  - <a class="text-decoration-none" href="#">Nam mauris velit</a>
+                </li>
+                <li>
+                  - <a class="text-decoration-none" href="#">Etiam vitae mauris</a>
+                </li>
+                <li>
+                  - <a class="text-decoration-none" href="#">Fusce scelerisque</a>
+                </li>
+                <li>
+                  - <a class="text-decoration-none" href="#">Sed faucibus</a>
+                </li>
+                <li>
+                  - <a class="text-decoration-none" href="#">Mauris efficitur nulla</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-4 col-xs-12 location text-white">
+              <h4 className="mt-lg-0 mt-sm-4">Location</h4>
+              <p>222/2, Islampur, Dhaka-1100</p>
+              <p className="mb-0">
+                <i className="fa fa-phone mr-3"></i>(02) 0034-0014
+              </p>
+              <p>
+                <i className="fa fa-envelope-o mr-3"></i>info@yourvirtualteacher
+              </p>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col copyright">
+              <p className="">
+                <small className="text-white-50">
+                  © 2022. All Rights Reserved.
+                </small>
+              </p>
+            </div>
+          </div>
         </div>
-        <div className='app-link'>
-        <h4 className='text-white text-muted'>Get the app</h4>
-        <div className='border border-white rounded-3 d-flex align-items-center'>
-
-        </div>
-        </div>
-        
-        </div>
-        </footer>
-    );
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
