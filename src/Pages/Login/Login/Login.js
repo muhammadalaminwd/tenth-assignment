d8f41af383b8bcaeb29c0f2404f1e12b79e08506
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-04-18 03:12:51 +0600

    login-page-modified

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -76,7 +76,7 @@ const resetPassword = async() => {
         </Button>
       </Form>
       {errorElement}
-      <p>New to Genius Car <Link to="/register" className="text-primary pe-auto text-decoration-none" onClick={navigateRegister}>Please Register</Link></p>
+      <p>Are you new here? <Link to="/register" className="text-primary pe-auto text-decoration-none" onClick={navigateRegister}>Please Register</Link></p>
       <p>Forget Password <button className="btn btn-link pe-auto text-decoration-none" onClick={resetPassword}>Reset Password</button></p>
       <SocialLogin></SocialLogin>
       <ToastContainer />
